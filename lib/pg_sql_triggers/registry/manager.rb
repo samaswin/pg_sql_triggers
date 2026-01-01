928128538562bc72d9da6004ae4581f512b09537
samaswin <aswin.r312@gmail.com>
2026-01-01 21:34:25 +0530

    Fixed the cache

--- lib/pg_sql_triggers/registry/manager.rb
+++ lib/pg_sql_triggers/registry/manager.rb
@@ -55,17 +55,27 @@ module PgSqlTriggers
           }
 
           if existing
-            begin
-              existing.update!(attributes)
-              # Update cache with the modified record (reload to get fresh data)
-              reloaded = existing.reload
-              _registry_cache[trigger_name] = reloaded
-              reloaded
-            rescue ActiveRecord::RecordNotFound
-              # Cached record was deleted, create a new one
-              new_record = PgSqlTriggers::TriggerRegistry.create!(attributes)
-              _registry_cache[trigger_name] = new_record
-              new_record
+            # Check if attributes have actually changed to avoid unnecessary queries
+            attributes_changed = attributes.any? do |key, value|
+              existing.send(key) != value
+            end
+
+            if attributes_changed
+              begin
+                existing.update!(attributes)
+                # Update cache with the modified record (reload to get fresh data)
+                reloaded = existing.reload
+                _registry_cache[trigger_name] = reloaded
+                reloaded
+              rescue ActiveRecord::RecordNotFound
+                # Cached record was deleted, create a new one
+                new_record = PgSqlTriggers::TriggerRegistry.create!(attributes)
+                _registry_cache[trigger_name] = new_record
+                new_record
+              end
+            else
+              # No changes, return cached record without any queries
+              existing
             end
           else
             new_record = PgSqlTriggers::TriggerRegistry.create!(attributes)
